371e6e81973c78e1842565c6da0ac802ad114c15
LawsonLamb <lawlzlamb@gmail.com>
2017-02-17 11:05:09 -0500

    Big 0

--- src/com/company/Big0/AdditionalProblems.java
+++ src/com/company/Big0/AdditionalProblems.java
@@ -4,4 +4,34 @@ package com.company.Big0;
  * Created by @Lawson Lamb  on 2/16/2017.
  */
 public class AdditionalProblems {
+    // The following code computes the product of a and b. What is its Runtime?
+    public int product(int a, int b){
+        int sum =0;
+        for(int i=0;i<b;i++){
+            sum+=a;
+        }
+        return sum;
+    }
+
+    // The following code computes a^b. What is its RunTime
+    public int power(int a,int b){
+        if(b<0){
+            return 0; //error
+        } else if(b ==0){
+            return 1;
+        }
+        else{
+            return a * power(a,b-1);
+        }
+    }
+
+    // The following code computes a%b What is its runtime
+    public int mod(int a, int b){
+        if(b<=0){
+            return -1;
+        }
+        int div = a/b;
+        return a - div*b;
+    }
+
 }
